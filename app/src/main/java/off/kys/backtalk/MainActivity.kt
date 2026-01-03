f9df356d376fbafd4ace327f9bea08cd8fcb515a
kys0ff <idk683120@gmail.com>
2026-01-03 10:45:19 +0100

    Initial commit

---
+++ app/src/main/java/off/kys/backtalk/MainActivity.kt
@@ -0,0 +1,41 @@
+package off.kys.backtalk
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import off.kys.backtalk.ui.theme.BacktalkTheme
+import org.koin.androidx.compose.koinViewModel
+import org.koin.compose.koinInject
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BacktalkTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = stringResource(R.string.messages))
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
