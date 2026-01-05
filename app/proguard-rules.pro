1882ffeb303576f014fb255c8bbba33e562d8203
kys0ff <idk683120@gmail.com>
2026-01-05 11:30:39 +0100

    feat: Enable Proguard and delete unused DatabaseProvider

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -19,3 +19,8 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+
+# Room specific rules
+-keepclassmembers class * extends androidx.room.RoomDatabase {
+   <init>(...);
+}
--- app/src/main/java/off/kys/backtalk/data/local/entity/MessageEntity.kt
+++ app/src/main/java/off/kys/backtalk/data/local/entity/MessageEntity.kt
@@ -1,10 +1,12 @@
 package off.kys.backtalk.data.local.entity
 
+import androidx.annotation.Keep
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import off.kys.backtalk.domain.model.MessageId
 
+@Keep
 @Entity(tableName = "messages")
 data class MessageEntity(
     @PrimaryKey
