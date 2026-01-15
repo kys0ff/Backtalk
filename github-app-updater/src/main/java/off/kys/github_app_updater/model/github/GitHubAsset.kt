cd37748d4fc7cb4a0f7a641f068c3ed4d02376e2
kys0ff <idk683120@gmail.com>
2026-01-15 12:00:54 +0100

    docs: Add KDoc to public models

--- github-app-updater/src/main/java/off/kys/github_app_updater/model/github/GitHubAsset.kt
+++ github-app-updater/src/main/java/off/kys/github_app_updater/model/github/GitHubAsset.kt
@@ -2,8 +2,17 @@ package off.kys.github_app_updater.model.github
 
 import com.squareup.moshi.Json
 
+/**
+ * Represents a GitHub release.
+ */
 data class GitHubAsset(
+    /**
+     * The name of the asset.
+     */
     val name: String,
+    /**
+     * The URL to download the asset.
+     */
     @param:Json(name = "browser_download_url")
     val browserDownloadUrl: String
 )
--- github-app-updater/src/main/java/off/kys/github_app_updater/model/updater/UpdateResult.kt
+++ github-app-updater/src/main/java/off/kys/github_app_updater/model/updater/UpdateResult.kt
@@ -2,6 +2,13 @@ package off.kys.github_app_updater.model.updater
 
 import off.kys.github_app_updater.model.github.GitHubAsset
 
+/**
+ * Represents the result of an update check.
+ *
+ * @param latestVersion The latest version of the app.
+ * @param changeLog The changelog for the update.
+ * @param downloadUrls The download URLs for the update.
+ */
 data class UpdateResult(
     val latestVersion: String,
     val changeLog: String,
