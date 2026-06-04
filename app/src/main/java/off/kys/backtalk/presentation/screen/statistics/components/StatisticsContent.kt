ae5b7cbbe1d1bf71b166337a57208b6251b7d269
kys0ff <idk683120@gmail.com>
2026-06-04 18:56:14 +0100

    ui: add top spacing to StatisticsContent

--- app/src/main/java/off/kys/backtalk/presentation/screen/statistics/components/StatisticsContent.kt
+++ app/src/main/java/off/kys/backtalk/presentation/screen/statistics/components/StatisticsContent.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -35,6 +36,7 @@ fun StatisticsContent(
             .padding(horizontal = 16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
+        Spacer(modifier = Modifier.size(16.dp))
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(12.dp)
