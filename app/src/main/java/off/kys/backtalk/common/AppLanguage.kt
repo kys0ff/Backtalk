eba2eb34f6d622b154d6927bfd33db8a7eeffa2a
kys0ff <idk683120@gmail.com>
2026-06-09 12:43:23 +0100

    feat #28: add Spanish localization

--- app/src/main/java/off/kys/backtalk/common/AppLanguage.kt
+++ app/src/main/java/off/kys/backtalk/common/AppLanguage.kt
@@ -2,14 +2,16 @@ package off.kys.backtalk.common
 
 import androidx.annotation.StringRes
 import off.kys.backtalk.R
+import off.kys.backtalk.util.emptyString
 
 enum class AppLanguage(
     val tag: String,
     @get:StringRes val displayNameRes: Int
 ) {
-    SYSTEM("", R.string.language_system),
+    SYSTEM(emptyString(), R.string.language_system),
     ENGLISH("en", R.string.language_english),
-    ARABIC("ar", R.string.language_arabic);
+    ARABIC("ar", R.string.language_arabic),
+    SPANISH("es", R.string.language_spanish);
 
     companion object {
         fun fromTag(tag: String?): AppLanguage {
