45cb5d2b6469cbf8835576b8e105828b55883557
kys0ff <idk683120@gmail.com>
2026-07-02 14:01:17 +0100

    refactor: introduce ignoreResult utility to explicitly discard expression returns

--- app/src/main/java/off/kys/backtalk/util/ignoreResult.kt
+++ app/src/main/java/off/kys/backtalk/util/ignoreResult.kt
@@ -2,7 +2,6 @@ package off.kys.backtalk.util
 
 /**
  * Discards the result of an expression and explicitly returns Unit.
- * Because typing ': Unit' or 'Unit' is a human rights violation.
  */
 inline fun ignoreResult(block: () -> Any?) {
     block()
